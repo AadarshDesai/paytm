97fd0339be9286094d6f8c468afd91897c198c34
Aadarsh Desai <adidesai91@gmail.com>
2024-11-08 03:34:10 -0500

    Added Frontend Code

---
+++ frontend/src/components/Appbar.jsx
@@ -0,0 +1,11 @@
+export function Appbar (){
+    return <div>
+        <div className="flex justify-between shadow h-14">
+            <div className="flex flex-col justify-center h-full ml-4">Paytm App</div>
+            <div className="flex">
+                <div className="flex flex-col justify-center h-full mr-4">Hello</div>
+                <div className="flex-flex-col justify-center h-full text-xl">U</div>
+            </div>
+        </div>
+    </div>
+}
